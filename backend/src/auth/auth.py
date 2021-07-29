7d01c6192e102d0cd13b0caacbaca42f730885cd
Pinal Buha <52285344+Pinalbuha@users.noreply.github.com>
2021-07-28 22:52:29 -0400

    Update auth.py

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'pinal9.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'http://localhost:5000'
+API_AUDIENCE = 'coffee'
 
 ## AuthError Exception
 '''
